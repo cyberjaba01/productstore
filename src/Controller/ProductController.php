1a83fd48a2d5b114f9212f23c71e0048e31345e6
cyberjaba01 <temoiiia123@gmail.com>
2025-06-23 18:21:14 +0300

    Added comment to test local brenches

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -20,6 +20,8 @@ final class ProductController extends AbstractController
     #[Route('/products/{id<\d+>}')]
     public function showItem($id, ProductRepository $repository): Response
     {
+        // added comment
+
         return $this->render('product/item.html.twig', [
             'product' => $repository->find($id) ?: throw $this->createNotFoundException('Product not found'),
         ]);
